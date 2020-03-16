531b6bc1c214dd6b3d0a0b387ea8bd61572e1b69
kaizhe <derek0405@gmail.com>
2020-03-16 16:03:37 -0700

    update entrypoint
    Signed-off-by: kaizhe <derek0405@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,23 +9,25 @@ TRUSTED_BASE_IMAGES=${4-""}
 SECRET_PATTERNS=${5-""}
 
 if [ "$DIRECTORY" = "" ]; then
-  $DIRECTORY="./"
+  DIRECTORY="./"
 fi
 
 if [ "$DOCKERFILE_PATTERN" = "" ]; then
-  $DOCKERFILE_PATTERN="dockerfile"
+  DOCKERFILE_PATTERN="dockerfile"
 fi
 
+CMD="/dockerfile-benchmarker --directory $DIRECTORY --dockerfile-pattern $DOCKERFILE_PATTERN"
+
 if [ "$DISALLOWD_PACKAGES" = "" ]; then
-  $DISALLOWD_PACKAGES="\"\""
+  CMD+=" --disallowed-packages $DISALLOWD_PACKAGES"
 fi
 
 if [ "$TRUSTED_BASE_IMAGES" = "" ]; then
-  $TRUSTED_BASE_IMAGES="\"\""
+  CMD+=" --trusted-base-images $TRUSTED_BASE_IMAGES"
 fi
 
 if [ "$SECRET_PATTERNS" = "" ]; then
-  $SECRET_PATTERNS="\"\""
+  CMD+=" --secret-patterns $SECRET_PATTERNS"
 fi
 
 echo "Directory: $DIRECTORY"
@@ -34,7 +36,9 @@ echo "Disallowed Packages: $DISALLOWD_PACKAGES"
 echo "Trusted base images: $TRUSTED_BASE_IMAGES"
 echo "Secret patterns: $SECRET_PATTERNS"
 
-report="$(/dockerfile-benchmarker --directory "$DIRECTORY" --dockerfile-pattern "$DOCKERFILE_PATTERN" --disallowed-packages "$DISALLOWD_PACKAGES" --trusted-base-images "$TRUSTED_BASE_IMAGES" --secret-patterns "$SECRET_PATTERNS")"
+echo "$CMD"
+
+report="$(CMD)"
 
 echo $report
 
