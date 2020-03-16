2176f0a8920944ba2c8d90f7a04b5bbffb41f5f9
kaizhe <derek0405@gmail.com>
2020-03-16 16:43:16 -0700

    last fix
    Signed-off-by: kaizhe <derek0405@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,7 +24,7 @@ echo "Trusted base images: $TRUSTED_BASE_IMAGES"
 echo "Secret patterns: $SECRET_PATTERNS"
 
 
-report="$("/dockerfile-benchmarker --directory \""$DIRECTORY"\" --dockerfile-pattern \""$DOCKERFILE_PATTERN"\" --disallowed-packages \""$DISALLOWD_PACKAGES"\" --trusted-base-images \""$TRUSTED_BASE_IMAGES"\" --secret-patterns \""$SECRET_PATTERNS"\"")"
+report=$(/dockerfile-benchmarker --directory \""$DIRECTORY"\" --dockerfile-pattern \""$DOCKERFILE_PATTERN"\" --disallowed-packages \""$DISALLOWD_PACKAGES"\" --trusted-base-images \""$TRUSTED_BASE_IMAGES"\" --secret-patterns \""$SECRET_PATTERNS"\")
 
 echo $report
 
