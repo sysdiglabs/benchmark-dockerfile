e96bf0e319940cac4c96ac7b9d9c9dc1fca0ac0c
kaizhe <derek0405@gmail.com>
2020-03-16 15:27:57 -0700

    update input parameters
    Signed-off-by: kaizhe <derek0405@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -eu
 
 DIRECTORY=${1-""}
 DOCKERFILE_PATTERN=${2-""}
+DISALLOWD_PACKAGES=${3-""}
+TRUSTED_BASE_IMAGES=${4-""}
+SECRET_PATTERNS=${5-""}
 
 if [ "$DIRECTORY" = "" ]; then
   $DIRECTORY="./"
@@ -16,7 +19,7 @@ fi
 echo "Directory: $DIRECTORY"
 echo "Dockerfile pattern: $DOCKERFILE_PATTERN"
 
-report="$(/dockerfile-benchmarker --directory "$DIRECTORY" --dockerfile-pattern "$DOCKERFILE_PATTERN")"
+report="$(/dockerfile-benchmarker --directory "$DIRECTORY" --dockerfile-pattern "$DOCKERFILE_PATTERN")" --disallowed-packages "$DISALLOWD_PACKAGES" --trusted-base-images "$TRUSTED_BASE_IMAGES" --secret-patterns "$SECRET_PATTERNS"
 
 echo $report
 
